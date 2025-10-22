918e7931c0760404e8941fa09bf7388f754c87d4
JeffersonSenaa <jeffersonsena12144@gmail.com>
2025-10-22 13:33:40 -0300

    Adicionando metodo para gerar rodadas

--- sorteio/sorteio_rodadas.py
+++ sorteio/sorteio_rodadas.py
@@ -20,5 +20,25 @@ class SorteioRodadas:
         self.times = times.copy()
 
     def gerar_rodadas(self):
-        pass
+        """
+        Gera todas as rodadas do campeonato usando o algoritmo Round-Robin:
+        - fixa um time na primeira posição
+        - rotaciona os demais times em sentido horário
+        - a cada rodada, emparelha os times em posições opostas
+        - alterna mandante e visitante para variar os jogos
+        - depois do turno, inverte mandante e visitante para o returno
+        """
+        num_times = len(self.times)
+        num_rodadas_turno = num_times - 1
+        
+        times_rotacao = self.times.copy()
+        
+        turno = []
+        for rodada_num in range(num_rodadas_turno):
+            rodada = []
+            
+            turno.append(rodada)
+            
+            # Rotacionar times 
+            times_rotacao = [times_rotacao[0]] + [times_rotacao[-1]] + times_rotacao[1:-1]
 
